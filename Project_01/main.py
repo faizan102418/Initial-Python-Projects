e135e431bc616345d039685717444e67ca895e9d
faizan102418 <m.faizansajid102418@gmail.com>
2024-10-25 07:44:13 +0500

    Update main.py

--- Project_01/main.py
+++ Project_01/main.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+# Bageles Game Number Gussing game
+"""Bagels, by Al Sweigart al@inventwithpython.com
+ 2. A deductive logic game where you must guess a number based on clues.
+ 3. View this code at https://nostarch.com/big-book-small-python-projects
+ 4. A version of this game is featured in the book "Invent Your Own
+ 5. Computer Games with Python" https://nostarch.com/inventwithpython
+ 6. Tags: short, game, puzzle"""
